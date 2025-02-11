2ad51829f88b46908229186fce9478dc3a904f54
SeaHuyty <seahuyty4444@gmail.com>
2025-02-11 19:27:34 +0700

    Updated files with new changes

--- Class/Supplier.java
+++ Class/Supplier.java
@@ -1,16 +1,35 @@
 package Class;
+import java.util.ArrayList;
+
 public class Supplier {
-    protected String name;
-    protected String contactInfo;
-    // this is the constructor
-    public Supplier(String name, String contactInfo) {
+    private int id;
+    private String name;
+    private String phoneNumber;
+    private String email;
+    private String address;
+    private ArrayList<Clothes> suppliedColthes;
+
+    public Supplier(int id, String name, String phoneNumber, String email, String address) {
+        this.id = id;
         this.name = name;
-        this.contactInfo = contactInfo;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.address = address;
+        this.suppliedColthes = new ArrayList<>();
     }
 
-    public String to_String(){
-        String text;
-        text = "Supplier Name: " + name + "\nSupplier Contact Info: " + contactInfo;
-        return text;
+    public Supplier(int id, String name, String phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = "Blank";
+        this.address = "Blank";
+        this.suppliedColthes = new ArrayList<>();
+    }
+
+    @Override
+    public String toString() {
+        return ("ID: " + this.id +
+            "\n");
     }
 }
