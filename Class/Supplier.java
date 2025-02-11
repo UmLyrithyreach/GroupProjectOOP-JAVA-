0adce42055df6fcbcbd65950a0ebaab794b67f51
SeaHuyty <seahuyty4444@gmail.com>
2025-02-11 19:58:40 +0700

    Did some modifications

--- Class/Supplier.java
+++ Class/Supplier.java
@@ -7,7 +7,7 @@ public class Supplier {
     private String phoneNumber;
     private String email;
     private String address;
-    private ArrayList<Clothes> suppliedColthes;
+    private ArrayList<Clothes> suppliedClothes;
 
     public Supplier(int id, String name, String phoneNumber, String email, String address) {
         this.id = id;
@@ -15,7 +15,7 @@ public class Supplier {
         this.phoneNumber = phoneNumber;
         this.email = email;
         this.address = address;
-        this.suppliedColthes = new ArrayList<>();
+        this.suppliedClothes = new ArrayList<>();
     }
 
     public Supplier(int id, String name, String phoneNumber) {
@@ -24,12 +24,30 @@ public class Supplier {
         this.phoneNumber = phoneNumber;
         this.email = "Blank";
         this.address = "Blank";
-        this.suppliedColthes = new ArrayList<>();
+        this.suppliedClothes = new ArrayList<>();
+    }
+
+    public void addClothes(Clothes clothes) {
+        suppliedClothes.add(clothes);
     }
 
     @Override
     public String toString() {
-        return ("ID: " + this.id +
-            "\n");
+        String result = "ID: " + this.id + "\n";
+        result += "Name: " + this.name + "\n";
+        result += "Phone Number: " + this.phoneNumber + "\n";
+        result += "Email: " + this.email + "\n";
+        result += "Address: " + this.address + "\n";
+        result += "Supplied Clothes:\n";
+
+        if (suppliedClothes.isEmpty()) {
+            result += "No clothes supplied.\n";
+        } else {
+            for (Clothes clothes: suppliedClothes) {
+                result += clothes.toString() + "\n";
+            }
+        }
+        
+        return result;
     }
 }
--- Main.java
+++ Main.java
@@ -5,7 +5,5 @@ public class Main {
     public static void main(String[] args) {
         Cloth duh = new Cloth(null, null, 0, null, 0);
         System.out.println(duh.to_String());
-        Supplier supplier = new Supplier("Duh", "1234567890");
-        System.out.println(supplier.to_String());
     }
 }
