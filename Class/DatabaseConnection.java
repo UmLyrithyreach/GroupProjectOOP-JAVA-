89d1621851a40edfb0556671ae288b9d78ad10a9
Kaizen06 <umlyrithyreach@gmail.com>
2025-03-20 13:51:25 +0700

    Deleting Membership

--- Class/DatabaseConnection.java
+++ Class/DatabaseConnection.java
@@ -4,9 +4,9 @@ import java.sql.*;
 
 public class DatabaseConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/clothing_store";
+    private static final String URL = "jdbc:mysql://localhost:3306/javadb";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "123abc";
+    private static final String PASSWORD = "Rithyreach@6780";
 
     // Establish a new connection for each query execution
     static Connection getConnection() throws SQLException {
